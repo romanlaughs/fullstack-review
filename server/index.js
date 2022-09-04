e1e9097c9de18afa38949bfa3de55f0b834e0cd2
Nick Cassano <romanlaughs@gmail.com>
2022-09-04 15:24:30 -0700

    added

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ let helper = require('../helpers/github.js');
 const { StringDecoder } = require('node:string_decoder');
 const decoder = new StringDecoder('utf8');
 const db = require('../database/index.js');
-const PORT = 1128
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.urlencoded({extended: true}));
